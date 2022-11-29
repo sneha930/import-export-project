305c3c889b2dde8742a1faf9711e9ef953d3d04f
sneha <sangaonkarsneha@gmail.com>
2022-11-29 21:49:27 +0530

    Initial commit

---
+++ src/components/InputQuery.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function InputQuery() {
+    return (
+        <input placeholder={'Type your query here'}></input>
+    )
+}
---
+++ src/components/SubHeading.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function SubHeading() {
+    return (
+        <h5>we are here to solve all your queries</h5>
+    )
+}
